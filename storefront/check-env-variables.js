e2ff9468a28fe29aa12bdaec8da6e1eae7322495
lawebdelcolchon <105754172+lawebdelcolchon@users.noreply.github.com>
2025-08-05 17:41:08 +0200

    Update check-env-variables.js

--- storefront/check-env-variables.js
+++ storefront/check-env-variables.js
@@ -1,5 +1,3 @@
-const c = require("ansi-colors")
-
 const requiredEnvs = [
   {
     key: "NEXT_PUBLIC_MEDUSA_BACKEND_URL",
@@ -26,33 +24,27 @@ const requiredEnvs = [
     description:
       "Your Stripe public key. See - https://docs.medusajs.com/add-plugins/stripe.",
   },
-]
+];
 
 function checkEnvVariables() {
-  const missingEnvs = requiredEnvs.filter(function (env) {
-    return !process.env[env.key]
-  })
+  const missingEnvs = requiredEnvs.filter((env) => !process.env[env.key]);
 
   if (missingEnvs.length > 0) {
-    console.error(
-      c.red.bold("\n🚫 Error: Missing required environment variables\n")
-    )
+    console.error("\n🚫 Error: Missing required environment variables\n");
 
-    missingEnvs.forEach(function (env) {
-      console.error(c.yellow(`  ${c.bold(env.key)}`))
+    missingEnvs.forEach((env) => {
+      console.error(`  ${env.key}`);
       if (env.description) {
-        console.error(c.dim(`    ${env.description}\n`))
+        console.error(`    ${env.description}\n`);
       }
-    })
+    });
 
     console.error(
-      c.yellow(
-        "\nPlease set these variables in your .env file or environment before starting the application.\n"
-      )
-    )
+      "\nPlease set these variables in your .env file or environment before starting the application.\n"
+    );
 
-    process.exit(1)
+    process.exit(1);
   }
 }
 
-module.exports = checkEnvVariables
+module.exports = checkEnvVariables;
